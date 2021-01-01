745f3864c619096abd5eeaf36b1c5a8ecc1305bd
rajesh.dnp01@gmail.com <rajesh.dnp01@gmail.com>
2021-01-01 18:19:54 +0530

    Add project files.

---
+++ WebApplication1/AccountController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login([FromBody] User login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+
+
+        private User AuthenticateUser(User login)
+        {
+            User user = null;
+
+            //Validate the User Credentials    
+            //Demo Purpose, I have Passed HardCoded User Information    
+            if (login.Email == "rajesh")
+            {
+                user = new User { Email = "rajesh@gmail.com", Password = "rajesh@gmail" };
+            }
+            return user;
+        }
+
+
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            bool result = false;
+
+            if (currentUser.HasClaim(c => c.Type == "Member_of_management"))
+            {
+                 result = Convert.ToBoolean(currentUser.Claims.FirstOrDefault(c => c.Type == "Member_of_management").Value);
+            }
+
+            if (result)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+
+
+
+        private string GenerateJSONWebToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("Member_of_management","true")
+        };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+    }
+}
